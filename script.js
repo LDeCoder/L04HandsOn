4c5f2e265e7fef9015a87c855340cfb2b78ba99c
LoriDeCoder <ldavis1005@gmail.com>
2018-07-06 16:03:24 -0500

    added provided information
    ready to begin.

---
+++ script.js
@@ -0,0 +1,17 @@
+class Employee {
+    constructor(name, salary, hireDate) {
+      this.name = name;
+      this.salary = salary;
+      this.hireDate = hireDate;
+    }
+    getName() {
+      console.log(this.name.toUpperCase());
+    }
+    getSalary() {
+      console.log(this.salary);
+    }
+    getHireDate() {
+      console.log(this.hireDate);
+    }
+  }
+  
