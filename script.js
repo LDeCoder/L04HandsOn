2d0ca5df447687bae8c1498fef761c4d5e3eaf4f
LoriDeCoder <ldavis1005@gmail.com>
2018-07-06 17:30:32 -0500

    console.log entered
    compiled console.log info.  Ready to instantiate...here goes nothing!! :)

--- script.js
+++ script.js
@@ -22,17 +22,15 @@ class Employee {
           this.jobDescription = jobDescription;
       }
       jobDescription(descriptionOfJob) {
-          console.log(use THIS name, hire date, salary and their job description - add a job description in line 25);
-      }
+          console.log(this.name + " was hired on " + this.hireDate + " and makes " this.salary + " because they " + this.jobDescription);
   }
-
   class designer extends Employee {
       constructor(yearsExperience, name, salary, hireDate) {
           super(name, salary, hireDate);
           this.yearsExperience = yearsExperience;
       }
       yearsExperience (experience) {
-          console.log ( USE THIS name, hire date, salary and their years of experience - add years experience to line 35);
+          console.log (this.name + " was hired on " + this.hireDate + " and makes " this.salary + " because they have " + this.yearsExperience);
       }
   }
 
@@ -41,9 +39,11 @@ class Employee {
             super(name, salary, hireDate);
             this.degreeCompleted = degreeCompleted;
         }
-        degreeCompleted (degrees){
-            console.log(USE THIS name, hire date, salary and the degree they have completed - add a degree completed to line 45);
+        degreeCompleted (degree){
+            console.log(this.name + " was hired on " + this.hireDate + " and makes " this.salary + " because they have " + this.degree);
+
         }
 
-  }
+  } 
+
   
