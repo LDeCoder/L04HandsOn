41fc680e5282b00bd8612f577b66895b4af95bd0
LoriDeCoder <ldavis1005@gmail.com>
2018-07-06 23:53:09 -0500

    ready for cleanup
    Everything functioning on its own.  Needs cleaned up.

--- script.js
+++ script.js
@@ -21,16 +21,17 @@ class Employee {
           super(name, salary, hireDate);
           this.jobDescription = jobDescription;
       }
-      jobDescription(descriptionOfJob) {
-          console.log(this.name + " was hired on " + this.hireDate + " and makes " this.salary + " because they " + this.jobDescription);
+      descriptionOfJob(){
+          console.log(this.name + " was hired on " + this.hireDate + " and makes " + this.salary + " because " + this.jobDescription);
   }
-  class designer extends Employee {
+  
+  class Designer extends Employee {
       constructor(yearsExperience, name, salary, hireDate) {
           super(name, salary, hireDate);
           this.yearsExperience = yearsExperience;
       }
-      yearsExperience (experience) {
-          console.log (this.name + " was hired on " + this.hireDate + " and makes " this.salary + " because they have " + this.yearsExperience);
+      Experience() {
+          console.log (this.name + " was hired on " + this.hireDate + " and makes " + this.salary + " because " + this.yearsExperience);
       }
   }
 
@@ -39,11 +40,25 @@ class Employee {
             super(name, salary, hireDate);
             this.degreeCompleted = degreeCompleted;
         }
-        degreeCompleted (degree){
-            console.log(this.name + " was hired on " + this.hireDate + " and makes " this.salary + " because they have " + this.degree);
+        Degree() {
+            console.log (this.name + " was hired on " + this.hireDate + " and makes " + this.salary + " because " + this.degreeCompleted);
 
-        }
-
-  } 
-
+  }
+}
   
+  let Fred = new Manager("he is everyone's boss.", "Fred Flintstone", 99000, "8/1/2005");
+  let Barney = new Designer("he has been here for 20 years!", "Barney Rubble" , 75000, "3/15/2008");
+let Betty = new salesAssociate("she has a masters in sales!", "Betty Rubble", 50000, "2/1/2015");
+
+
+
+Fred.descriptionOfJob();
+Barney.Experience();
+Betty.Degree();
+
+
+
+
+
+//   - [ ] Lastly, using each of the new instances, call each of the methods that live within the respective subclasses so you can see each of the console.logs in your console.
+//     - [ ] Use the http-server to run this project
